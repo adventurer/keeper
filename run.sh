9f5be4147d892d5f8b73ab2d99d01635da795923
adventurer <1550520500@qq.com>
2020-09-04 09:52:31 +0800

    1

--- run.sh
+++ run.sh
@@ -4,10 +4,13 @@
 echo "begin keeper..."
 /root/keeper/keeper -name=$SERVERNAME -ip=$IP -port=$SERVERPORT > /var/log/keeper.log 2>&1 &
 echo "wait for 5 second..."
-sleep 5
+sleep 2
 echo "begin ss-server..."
 # start 2
 /bin/ss-server -c config.json -u > /var/log/ss-server.log 2>&1 &
+
+echo "begin goiftop..."
+/root/keeper/goiftop -i eth0 2>&1 &
 
 echo "done..."
 # just keep this script running
